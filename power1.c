20efb881d574c2c3cfb021f5c3e1d70719711550
15euec076 <32997235+15euec076@users.noreply.github.com>
2017-10-22 23:55:04 +0530

    Rename power.c to power1.c

---
+++ power1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	 int base,expo,result;
+	 scanf("%d",&base);
+	 scanf("%d",&expo);
+	 
+	 
+	result= pow(base, expo);
+	 
+	 printf("%d",result);
+
+	return 0;
+}
